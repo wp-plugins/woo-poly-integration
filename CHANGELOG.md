492b4fe8b6f9bd744ea5fce94a28e5f70f6a8f27
hyyan <hyyan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 13:08:40 +0000

    Tagged version 0.19
    git-svn-id: https://plugins.svn.wordpress.org/woo-poly-integration/tags/0.19@1156129 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ CHANGELOG.md
@@ -0,0 +1,97 @@
+###0.19
+
+* Added the ability to set the write permalinks that can work with polylang if the default woocomerce permalinks are used
+
+###0.18
+
+* Added basic support for reports (filter by language)
+* General fixes
+
+###0.17.2
+
+* Fixed issue#2 (https://github.com/hyyan/woo-poly-integration/issues/2)
+
+###0.17.1
+
+* Removed wrong php used statement
+
+###0.17
+
+* Fixed (Polylang language switcher is disabled even if there is no variable products)
+* Added the ability to sync product category custom fields
+
+###0.16
+
+* Added support for product gallery translation
+
+###0.15
+
+* Extended meta list to include _visibility
+
+###0.14
+
+* Made ready to release to wordpress
+
+###0.13
+
+* Coupons support variation product
+
+###0.12
+
+* Added support for emails
+
+###0.11.2
+
+* Added forgotten return statement
+
+###0.11.1
+
+* Corrected query used to get the orders list in my account page
+
+###0.11
+
+* Improved support for product variations 
+* Imporved support for orders
+
+###0.10
+
+* Added the ability to handle shop page translation
+
+###0.9
+
+* Refactoring Code 
+* Added support for variation products
+
+
+###0.8
+
+* Refactoring Code 
+* General Improvements
+
+###0.7
+
+* Added support for attributes translation
+
+###0.6
+
+* Added support for order translation
+
+###0.5
+
+* Added support for coupon translation
+
+###0.4
+
+* General improvements for product meta sync
+
+###0.3
+
+* added porduct restoreStockQuantity function
+
+###0.2
+
+* Added product meta and stock sync support
+
+###0.1
+
+* Initial commit
---
+++ src/Hyyan/WPI/Plugin.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hyyan\WPI;
+
+/**
+ * Plugin
+ *
+ * @author Hyyan
+ */
+class Plugin
+{
+
+    /**
+     * Construct the plugin
+     */
+    public function __construct()
+    {
+        add_action('init', array($this, 'activate'));
+
+        /* Registered anyway */
+        new Emails();
+    }
+
+    /**
+     * Activate plugin
+     *
+     * The plugin will register its core if the requirements are full filled , otherwise
+     * it will show an admin error message
+     *
+     * @return boolean false if plugin can not be activated
+     */
+    public function activate()
+    {
+        if (!static::canActivate()) {
+            add_action('admin_notices', function () {
+                printf('<div id="message" class="error"><p>%s</p></div>', __('Hyyan WooCommerce Polylang Integration Plugin can not function correctly , the plugin requires WooCommerce and Polylang plugins', 'woo-poly-integration'));
+            });
+
+            return false;
+        }
+
+        $this->registerCore();
+    }
+
+    /**
+     * Check if the plugin can be activated
+     *
+     * @return boolean true if can be activated , false otherwise
+     */
+    public static function canActivate()
+    {
+        $requiredPlugins = array(
+            'polylang/polylang.php',
+            'woocommerce/woocommerce.php'
+        );
+
+        $plugins = apply_filters('active_plugins', get_option('active_plugins'));
+
+        foreach ($requiredPlugins as $name) {
+            if (!in_array($name, $plugins)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get current plugin version
+     *
+     * @return integer
+     */
+    public static function getVersion()
+    {
+        $data = get_plugin_data(Hyyan_WPI_DIR);
+
+        return $data['Version'];
+    }
+
+    /**
+     * Add plugin core classes
+     */
+    protected function registerCore()
+    {
+        new Cart();
+        new Coupon();
+        new Login();
+        new Order();
+        new Pages();
+        new Product();
+        new Media();
+        new Reports();
+        new Permalinks();
+    }
+
+}
