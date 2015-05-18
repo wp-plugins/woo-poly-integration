dca5c86b1e0fb235a49a630ca37d5f3e7d7560cb
hyyan <hyyan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 20:07:31 +0000

    Released version 0.20
    git-svn-id: https://plugins.svn.wordpress.org/woo-poly-integration/tags/0.20@1162902 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,134 @@
+=== Hyyan WooCommerce Polylang Integration===
+Contributors: hyyan
+Tags: cms, commerce, e-commerce, e-shop, ecommerce, multilingual, products, shop, woocommerce, polylang ,bilingual, international, language, localization, multilanguage, multilingual, translate, translation
+Requires at least: 3.8
+Tested up to: 4.2
+Stable tag: 0.20
+License: MIT
+License URI: https://github.com/hyyan/woo-poly-integration/blob/master/LICENSE
+
+Integrates Woocommerce With Polylang 
+
+== Description ==
+
+This plugin makes it possible to run multilingual e-commerce sites using
+WooCommerce and Polylang.It makes products and store pages translatable, lets 
+visitors switch languages and order products in their language. and all that frome 
+the same interface you love.
+
+= Features  =
+
+- [√] Page Translation
+- [√] Product Translation
+  - [√] Categories
+  - [√] Tags
+  - [√] Attributes
+  - [√] Shipping Classes
+  - [√] Meta Synchronization
+  - [√] Variation Product
+  - [√] Product Gallery
+- [√] Order Translation
+- [√] Cart Synchronization `Without Variation Support`
+- [√] Coupon Synchronization
+- [√] Emails
+- [√] Reports
+  - [√] Filter by language
+  - [√] Combine reports for all languages
+
+
+= What you need to know about this plugin =
+
+1. The plugin support variable products , but using them will `disallow you to 
+  change the default language` , because of the way the plugin implements this
+  support. So you have to make sure to choose the default language before start
+  adding new variable products.
+
+= Setup your environment =
+
+1. Make sure to setup your woocommerce permalinks correctly
+2. You need to translate woocommerce pages by yourself
+3. The plugin will handle the rest for you
+
+= Contributing =
+
+Everyone is welcome to help contribute and improve this plugin. There are several
+ways you can contribute:
+
+* Reporting issues (please read [issue guidelines](https://github.com/necolas/issue-guidelines))
+* Suggesting new features
+* Writing or refactoring code
+* Fixing [issues](https://github.com/hyyan/woo-poly-integration/issues)
+
+== Installation ==
+
+= Classical way =
+
+1. Download the plugin as zip archive and then upload it to your wordpress plugins folder and
+extract it there.
+2. Activate the plugin from your admin panel
+
+= Composer way =
+
+1. run composer command : ``` composer require hyyan/woo-poly-integration```
+
+== Frequently Asked Questions ==
+
+= Does this work with other e-commerce plugins ? =
+
+No. This plugin is for polylang and woocommerce
+
+= Does this work with WPML plugin? =
+
+No. This plugin is for polylang and woocommerce
+
+= What do I need to do in my theme =
+
+Well , Nothing 
+
+= Products Category or tags pages are blank =
+
+Just make sure to setup your permalinks , and every thing will be fine , I promise
+
+
+== Screenshots ==
+
+1. Add and translate products from the same interface you love
+2. Products meta is synced , no need to do anything by your own
+3. Orders uses the customer chosen language 
+4. Orders language can be changed 
+5. Get reports in specific language and combine reports for all langauges
+
+== Changelog ==
+
+= 0.20 =
+* Added the ability to sync total_sales when stock value is changed
+* Added the ability to combine product report with its translation
+* Added the ability to combine category report with its translation
+* Fixed database error in sales_be_category reports
+* Fixed Orders Interface to use the current user language instead of the order language
+
+= 0.19 =
+* Added the ability to set the write permalinks that can work with polylang if the default woocomerce permalinks are used
+
+= 0.18 =
+* Added basic support for reports (filter by language)
+* General fixes
+
+= 0.17.2 =
+* Fixed issue#2 (https://github.com/hyyan/woo-poly-integration/issues/2)
+
+= 0.17.1 =
+* Removed wrong php used statement
+
+= 0.17 =
+* Fixed (Polylang language switcher is disabled even if there is no variable products)
+* Added the ability to sync product category custom fields
+
+= 0.16 =
+* Added support for product gallery translation
+
+= 0.15 =
+* Extended meta list to include _visibility
+
+= 0.14 =
+* Released in the wordpress repository
---
+++ src/Hyyan/WPI/Product/Variable.php
@@ -0,0 +1,246 @@
+<?php
+
+/**
+ * This file is part of the hyyan/woo-poly-integration plugin.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hyyan\WPI\Product;
+
+use Hyyan\WPI\HooksInterface;
+use Hyyan\WPI\Utilities;
+
+/**
+ * Variable
+ *
+ * Handle Variable Products
+ *
+ * @author Hyyan
+ */
+class Variable
+{
+
+    /**
+     * Construct object
+     */
+    public function __construct()
+    {
+
+        add_action('save_post', array($this, 'duplicateVariations'), 10, 3);
+        add_action(
+                'wp_ajax_woocommerce_remove_variations'
+                , array($this, 'removeVariations')
+                , 9
+        );
+
+        // extend meta list to include variation meta
+        add_filter(
+                HooksInterface::PRODUCT_META_SYNC_FILTER
+                , array($this, 'extendProductMetaList')
+        );
+
+        if (is_admin()) {
+            $this->handleVariableLimitation();
+            $this->shouldDisableLangSwitcher();
+        }
+    }
+
+    /**
+     * Translate Variation for given variable product
+     *
+     * @param integer  $ID     product variable ID
+     * @param \WP_Post $post   Product Post
+     * @param boolean  $update true if update , false otherwise
+     *
+     * @return boolean
+     */
+    public function duplicateVariations($ID, \WP_Post $post, $update)
+    {
+
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return false;
+        }
+
+        global $pagenow;
+        if (!in_array($pagenow, array('post.php', 'post-new.php'))) {
+            return false;
+        }
+
+        $product = wc_get_product($ID);
+
+        if (!$product) {
+            return false;
+        }
+
+        $from = null;
+
+        if (pll_get_post_language($ID) == pll_default_language()) {
+            $from = $product;
+        } else {
+
+            if (isset($_GET['from_post'])) {
+                /*
+                 * This check will make sure that variation , will be
+                 * created for brand new products which are not saved yet by user
+                 */
+                $from = Utilities::getProductTranslationByID(
+                                esc_attr($_GET['from_post'])
+                                , pll_default_language()
+                );
+            } else {
+                $from = Utilities::getProductTranslationByObject(
+                                $product
+                                , pll_default_language()
+                );
+            }
+        }
+
+        if (!($from instanceof \WC_Product_Variable)) {
+            return false;
+        }
+
+        $langs = pll_languages_list();
+
+        foreach ($langs as $lang) {
+            $variation = new Variation(
+                    $from
+                    , Utilities::getProductTranslationByObject($product, $lang)
+            );
+
+            remove_action('save_post', array($this, __FUNCTION__), 10);
+
+            $variation->duplicate();
+
+            add_action('save_post', array($this, __FUNCTION__), 10, 3);
+        }
+    }
+
+    /**
+     * Remove variatoins related to current removed variation
+     */
+    public function removeVariations()
+    {
+        if (isset($_POST['variation_ids'])) {
+            $IDS = (array) $_POST['variation_ids'];
+
+            foreach ($IDS as $ID) {
+                Variation::deleteRelatedVariation($ID);
+            }
+        }
+    }
+
+    /**
+     * Extend the product meta list that must by synced
+     *
+     * @param array $metas current meta list
+     *
+     * @return array
+     */
+    public function extendProductMetaList(array $metas)
+    {
+        return array_merge($metas, array(
+            '_min_variation_price',
+            '_max_variation_price',
+            '_min_price_variation_id',
+            '_max_price_variation_id',
+            '_min_variation_regular_price',
+            '_max_variation_regular_price',
+            '_min_regular_price_variation_id',
+            '_max_regular_price_variation_id',
+            '_min_variation_sale_price',
+            '_max_variation_sale_price',
+            '_min_sale_price_variation_id',
+            '_max_sale_price_variation_id',
+        ));
+    }
+
+    /**
+     * Handle variation limitation about defualt language
+     *
+     * @global string $pagenow current page name
+     *
+     * @return boolean false if this is not new variable product
+     */
+    public function handleVariableLimitation()
+    {
+
+        global $pagenow;
+        if ($pagenow !== 'post-new.php') {
+            return false;
+        }
+
+        if (isset($_GET['from_post'])) {
+            return false;
+        }
+
+        if (pll_current_language() === pll_default_language()) {
+            return false;
+        }
+
+        add_action('admin_print_scripts', function () {
+            printf(
+                    '<script type="text/javascript" id="woo-poly-variables-data">'
+                    . ' var HYYAN_WPI_VARIABLES = {'
+                    . '     title       : "%s" ,'
+                    . '     content     : "%s" ,'
+                    . '     defaultLang : "%s"'
+                    . ' };'
+                    . '</script>'
+                    , __('Wrong Language For Variable Product', 'woo-poly-integration')
+                    , __("Variable product must be created in the default language first or things will get messy. <br> <a href='https://github.com/hyyan/woo-poly-integration/tree/master#what-you-need-to-know-about-this-plugin' target='_blank'>Read more , to know why</a>", "woo-poly-integration")
+                    , pll_default_language()
+            );
+        });
+
+        add_action('admin_enqueue_scripts', function () {
+            wp_enqueue_script('jquery-ui-core');
+            wp_enqueue_script("jquery-effects-core");
+            wp_enqueue_script('jquery-ui-dialog');
+            wp_enqueue_script(
+                    'woo-poly-variables'
+                    , plugins_url('public/js/Variables.js', Hyyan_WPI_DIR)
+                    , array('jquery', 'jquery-ui-core', 'jquery-ui-dialog')
+                    , \Hyyan\WPI\Plugin::getVersion()
+                    , true
+            );
+        }, 100);
+    }
+
+    /**
+     * Check if we have to disable the language switcher in the polylang setting
+     * page
+     */
+    public function shouldDisableLangSwitcher()
+    {
+        add_action('current_screen', function () {
+            $screen = get_current_screen();
+            if ($screen->id !== 'settings_page_mlang') {
+                return false;
+            }
+            $count = wp_count_posts('product_variation');
+            if (!($count && $count->publish > 0)) {
+                return false;
+            }
+            add_action('admin_print_scripts', function () {
+                printf(
+                        '<script type="text/javascript" id="woo-poly-disable-lang-switcher">'
+                        . '  jQuery(document).ready(function ($) {'
+                        . '     $("#options-lang #default_lang")'
+                        . '         .css({
+                                        "opacity": .5,
+                                        "pointer-events": "none",
+                                        "cursor": "not-allowed"
+                                    });'
+                        . '     $("#options-lang").prepend("<p class=\'update-nag\'>%s</p>");'
+                        . '  });'
+                        . '</script>'
+                        , __('You can not change the default language ,Becuase you are using variable products', 'woo-poly-integration')
+                );
+            }, 100);
+        });
+    }
+
+}
